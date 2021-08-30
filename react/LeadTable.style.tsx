4719a86e46b8b9707c9420e61f36c7640a7b348a
Victor Gadioli <victorhgadioli@gmail.com>
2021-08-30 17:40:37 -0300

    Table updated

---
+++ react/LeadTable.style.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+import Table from "@material-ui/core/Table";
+import { Paper } from "@material-ui/core";
+
+export const Page = styled(Paper)`
+  background-color: #eeeeee;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const TableStyled = styled(Table)``;
+
+export const PaperStyled = styled(Paper)`
+  max-width: 1600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
--- react/LeadTable.tsx
+++ react/LeadTable.tsx
@@ -1,51 +1,84 @@
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-import { useQuery } from 'react-apollo'
-import GET_LEADS from './getLeads.graphql'
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import { useQuery } from "react-apollo";
+import GET_LEADS from "./queries/getLeads.graphql";
+
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import { Page, PaperStyled, TableStyled } from "./LeadTable.style";
+import { Button } from "@material-ui/core";
 
 interface LeadTableProps {
-  title: string
+  title: string;
 }
 
 interface leadData {
-  email: string
-  type: string
+  id?: string;
+  name?: string;
+  email?: string;
+  phoneNumber?: string;
+  type?: string;
 }
 
 interface queryData {
   leads: {
-      body: leadData[]
-  }
+    body: leadData[];
+  };
 }
 
-const LeadTable: StorefrontFunctionComponent<LeadTableProps> = ({title}) => {
-  const titleText = title || <FormattedMessage id="leadtable.title" />
+const LeadTable: StorefrontFunctionComponent<LeadTableProps> = ({ title }) => {
+  const titleText = title || <FormattedMessage id="leadtable.title" />;
 
-  const { loading, error, data, refetch } = useQuery<queryData>(GET_LEADS, 
-    {
-        ssr: false
-    })
+  const { loading, error, data, refetch } = useQuery<queryData>(GET_LEADS, {
+    ssr: false,
+  });
 
-  if (loading) console.log('Loading...')
-  if (error) console.log(`Error ${error}`)
+  if (loading) console.log("Loading...");
+  if (error) console.log(`Error ${error}`);
 
   return (
-    <>
-        <button onClick={() => refetch()}>Atualizar</button>
-        <ul>
-            {!loading && (data && data.leads.body.map((l: leadData, idx: number) => {
-                return (<li key={idx}>{l.email} | {l.type}</li>)
-            }))}
-        </ul>
-    </>
-  )
-}
+    <Page>
+      <Button variant="contained" onClick={() => refetch()}>Atualizar</Button>
+      <TableContainer component={PaperStyled}>
+        <TableStyled>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">ID</TableCell>
+              <TableCell align="center">Nome Completo</TableCell>
+              <TableCell align="center">E-mail</TableCell>
+              <TableCell align="center">Telefone</TableCell>
+              <TableCell align="center">Tipo</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {!loading &&
+              data &&
+              data.leads.body.map((l: leadData, idx: number) => {
+                return (
+                  <TableRow>
+                    <TableCell align="center">{l.id}</TableCell>
+                    <TableCell align="center">{l.name}</TableCell>
+                    <TableCell align="center">{l.email}</TableCell>
+                    <TableCell align="center">{l.phoneNumber}</TableCell>
+                    <TableCell align="center">{l.type}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </TableStyled>
+      </TableContainer>
+    </Page>
+  );
+};
 
 LeadTable.schema = {
-  title: 'editor.leadtable.title',
-  description: 'editor.leadtable.description',
-  type: 'object',
+  title: "editor.leadtable.title",
+  description: "editor.leadtable.description",
+  type: "object",
   properties: {},
-}
+};
 
-export default LeadTable
+export default LeadTable;
